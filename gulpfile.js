1bf77f90d264c10cd66866e1e6c4a6829311a4b6
Anton Vorobyov <igelness@gmail.com>
2019-08-11 05:28:47 +0300

    CONCAT javascript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,18 @@
-let gulp = require('gulp'),
-browserSync = require('browser-sync');
+let gulp = require("gulp"),
+  browserSync = require("browser-sync"),
+  concat = require("gulp-concat"),
+  uglify = require("gulp-uglify");
 
-
-gulp.task('watch', function(){
-  gulp.watch('app/*html', gulp.parallel('html'))
-  gulp.watch('app/js/*.js', gulp.parallel('script'))
+gulp.task("watch", function() {
+  gulp.watch("app/*html", gulp.parallel("html"));
+  gulp.watch("app/js/*.js", gulp.parallel("script"));
 });
 
-gulp.task('browser-sync', function() {
+gulp.task("browser-sync", function() {
   browserSync.init({
-      server: {
-          baseDir: "app/"
-      }
+    server: {
+      baseDir: "app/"
+    }
   });
 });
 
@@ -19,8 +20,22 @@ gulp.task("html", function() {
   return gulp.src("app/*.html").pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task("script", function(){
-  return gulp.src('app/js/*.js').pipe(browserSync.reload({stream:true}))
+gulp.task("script", function() {
+  return gulp.src("app/js/*.js").pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('default', gulp.parallel('browser-sync', 'watch'));
+gulp.task("js", function() {
+  return gulp
+    .src([
+      "app/js/slick.min.js",
+      "app/js/jquery.fancybox.min.js",
+      "app/js/jquery.smoothscroll.min.js",
+      "app/js/main.js"
+    ])
+    .pipe(concat("libs.min.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest("app/js/libs"))
+    .pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task("default", gulp.parallel("js", "browser-sync", "watch"));
